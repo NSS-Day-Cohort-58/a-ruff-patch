b799466b4bae838901ddf09106e218cddda2ed67
Devin <dalamcd@gmail.com>
2022-08-24 09:48:46 -0500

    Added fetch calls and getter functions for dogs and performances

--- scripts/state.js
+++ scripts/state.js
@@ -6,6 +6,7 @@ const applicationState = {
 
 const api = "http://localhost:8088"
 
+// Jokes
 export const fetchJokes = () => {
     return fetch(`${api}/jokes`)
         .then(response => response.json())
@@ -15,3 +16,25 @@ export const fetchJokes = () => {
 }
 
 export const getJokes = () => structuredClone(applicationState.jokes)
+
+// Dogs
+export const fetchDogs = () => {
+    return fetch(`${api}/dogs`)
+        .then(response => response.json())
+        .then((dogs) => {
+            applicationState.dogs = dogs
+        })
+}
+
+export const getDogs = () => structuredClone(applicationState.dogs)
+
+// Performances
+ export const fetchPerformances = () => {
+     return fetch(`${api}/performances`)
+     .then(response => response.json())
+     .then((performances) => {
+         applicationState.performances = performances
+     })
+ }
+ 
+ export const getPerformances = () => structuredClone(applicationState.performances)
